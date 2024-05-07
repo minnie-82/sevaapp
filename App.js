7ae4a45d9825d03282958bcd4706ff24e13ebf2f
Yashvi <yashvivaghasiya33@gmail.com>
2024-05-07 15:29:06 +0530

    android folder and eas.json file added

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default function App() {
     // <AdminSevaDetails></AdminSevaDetails>
     <NavigationContainer>
      {/* <AdminHome></AdminHome>  */}
-     <AdminHome/>
+     <Login/>
 
 
     </NavigationContainer>
