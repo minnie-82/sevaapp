d42468650050820761b0b196833786608c56433b
Charmi_Tank <charmi.git.23@gmail.com>
2024-05-07 22:28:05 +0530

    Sevacard user

--- src/components/global.js
+++ src/components/global.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useContext } from "react";
 
-export const API_ENDPOINT = "https://78fd-110-226-176-187.ngrok-free.app/";
+export const API_ENDPOINT = "https://73e2-1-38-140-11.ngrok-free.app/";
 
 
 // const UserContext = createContext();
