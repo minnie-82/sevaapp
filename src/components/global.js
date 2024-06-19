976ad5c9dbfe21644cbc189064d37a707c7491bf
17Akshar <aksharparmar2002@gmail.com>
2024-06-19 18:06:15 +0530

    bug fixed : minor bug fixed

--- src/components/global.js
+++ src/components/global.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useContext } from "react";
 
-export const API_ENDPOINT = "https://dccd-1-38-141-205.ngrok-free.app/";
+export const API_ENDPOINT = "https://34d6-59-152-53-130.ngrok-free.app";
 
 const UserContext = createContext();
 
